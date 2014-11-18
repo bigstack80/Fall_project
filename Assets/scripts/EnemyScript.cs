8147fc6927390022334d06af95986cf9d9f808c5
Adam Stack <bigstack80@knights.ucf.edu>
2014-11-18 02:38:16 -0500

    some beginning level design

--- Assets/scripts/EnemyScript.cs
+++ Assets/scripts/EnemyScript.cs
@@ -39,18 +39,18 @@ public class EnemyScript : MonoBehaviour
 		if (hp > 0) {
 			Vector3 line = transform.position - player.position;
 			double range = Math.Sqrt (Math.Pow ((double)line.x, 2) + Math.Pow ((double)line.y, 2));
-			if (range < 1.75) {
+			if (range < 5.75) {
 				transform.LookAt (player.position);
 				transform.Rotate (0, 90, 90);
 				float speed = 0.01F;
-				if (range <= 0.75) {
+				if (range <= 2.75) {
 					speed = 0.0F;
-				} else if (range <= 0.3) {
+				} else if (range <= 1) {
 					speed = -0.01F;
 				}
 				transform.Translate (0, speed, 0);
 			}
-			if (range <= 1.25) {
+			if (range <= 4) {
 
 				WeaponScript[] weapon = GetComponentsInChildren<WeaponScript> ();
 				foreach (WeaponScript w in weapon) {
